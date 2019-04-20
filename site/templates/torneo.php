6fd7c821332658a95d0ec367a2a8dcd726aa9c66
levihernan <levi.hernan@gmail.com>
2019-04-20 11:50:06 -0300

    solid bg

--- site/templates/torneo.php
+++ site/templates/torneo.php
@@ -61,7 +61,7 @@ p.proxDate {
 
 
   <div class="grid-container">
-    <div class="svgLoader">
+    <div class="svgLoader bitten">
 
       <svg version="1.1" id="L7" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
       viewBox="0 0 100 100" enable-background="new 0 0 100 100" xml:space="preserve">
